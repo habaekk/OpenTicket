a8f4b18aa331927bcc1af89e8a99fb4d8d3ebda4
habaekk <haback1493@gmail.com>
2024-11-16 18:30:29 +0900

    edit secheduleService to return dummy data

--- services/scheduleService.js
+++ services/scheduleService.js
@@ -2,12 +2,14 @@
 
 export const fetchSchedules = async () => {
     try {
-      const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}`);
-      if (!response.ok) {
-        throw new Error('Failed to fetch schedules');
-      }
-      const data = await response.json();
-      return data;
+      // const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}`);
+      // if (!response.ok) {
+      //   throw new Error('Failed to fetch schedules');
+      // }
+      // const data = await response.json();
+      // return data;
+
+      return dummySchedules;
     } catch (error) {
       console.error(error);
       return [];
