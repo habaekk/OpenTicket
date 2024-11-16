82b15babaf543af7718bbcb769ad963a663573fd
habaekk <haback1493@gmail.com>
2024-11-16 18:27:00 +0900

    add navbar component

---
+++ components/Navbar.js
@@ -0,0 +1,13 @@
+// components/Navbar.js
+
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <nav className="bg-blue-600 p-4 text-white">
+      <h1 className="text-xl font-bold">티켓 스케줄 뷰어</h1>
+    </nav>
+  );
+};
+
+export default Navbar;
