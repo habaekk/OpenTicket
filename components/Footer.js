cc599bc2761a648c06e8155def3c45beba8bae45
habaekk <haback1493@gmail.com>
2024-11-16 20:05:17 +0900

    edit footer

--- components/Footer.js
+++ components/Footer.js
@@ -5,7 +5,7 @@ import React from 'react';
 const Footer = () => {
   return (
     <footer className="bg-gray-100 p-4 text-center text-gray-600">
-      <p>&copy; 2024 OpenTicket</p>
+      <p>&copy; 2024 OpenTicket All rights reserved.</p>
     </footer>
   );
 };
